09e610ac42833f4063b4a3bf97da7b6b5ddf421f
Berke Kabagöz <berkekbgz@gmail.com>
2026-02-02 00:35:19 +0300

    ft_printf: initial commit

---
+++ 1_push_swap/1_ft_printf/ft_printf.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkabagoz <bkabagoz@student.42istanbul.com> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 19:24:43 by bkabagoz          #+#    #+#             */
+/*   Updated: 2026/02/02 00:31:20 by bkabagoz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include "ft_put/ft_put.h"
+#include "libft.h"
+#include <unistd.h>
+
+char	handle_formatting(const char *peek, va_list *args)
+{
+	if (*peek == '%')
+		return (write(1, "%", 1));
+	else if (*peek == 'c')
+		return (ft_putchar(va_arg(*args, int)));
+	else if (*peek == 's')
+		return (ft_putstr(va_arg(*args, char *)));
+	else if (*peek == 'p')
+		return (ft_putptr(va_arg(*args, void *)));
+	else if (*peek == 'd')
+		return (0);
+	else if (*peek == 'i')
+		return (0);
+	else if (*peek == 'u')
+		return (0);
+	else if (*peek == 'x')
+		return (ft_puthex(va_arg(*args, unsigned int), 0));
+	else if (*peek == 'X')
+		return (ft_puthex(va_arg(*args, unsigned int), 1));
+	return (0);
+}
+
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		ret;
+	size_t	count;
+
+	count = 0;
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format != '%')
+			ret = write(1, format++, 1);
+		else if (format[1] && ft_strchr("cspdiuxX%", format[1]))
+		{
+			ret = handle_formatting(format + 1, &args);
+			format += 2;
+		}
+		else
+			ret = write(1, format++, 1);
+		if (ret == -1)
+			return (-1);
+		count += ret;
+	}
+	return (count);
+}
