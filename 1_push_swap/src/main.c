3579dbe37e13972c4371e6d87a84dd8f3c797786
Eren Arslan <erearsla@k1m25s09.42istanbul.com.tr>
2026-03-13 17:17:31 +0300

    Main Düzenlendi

--- 1_push_swap/src/main.c
+++ 1_push_swap/src/main.c
@@ -5,50 +5,89 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: erearsla <erearsla@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/07 20:02:35 by erearsla          #+#    #+#             */
-/*   Updated: 2026/03/08 13:19:17 by bkabagoz         ###   ########.fr       */
+/*   Created: 2026/03/08 13:28:07 by erearsla          #+#    #+#             */
+/*   Updated: 2026/03/08 13:30:18 by erearsla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include "ft_printf.h"
-#include <stdlib.h>
 
+/*
+** Gelen argümanın hangi bayrak olduğunu bulup durumu günceller.
+*/
+static void	set_strategy(char *arg, t_state *state)
+{
+	if (ft_strncmp(arg, "--bench", 8) == 0)
+		state->bench_mode = 1;
+	else if (ft_strncmp(arg, "--simple", 9) == 0)
+		state->strategy_flag = 1;
+	else if (ft_strncmp(arg, "--medium", 9) == 0)
+		state->strategy_flag = 2;
+	else if (ft_strncmp(arg, "--complex", 10) == 0)
+		state->strategy_flag = 3;
+	else if (ft_strncmp(arg, "--adaptive", 11) == 0)
+		state->strategy_flag = 0;
+}
+
+/*
+*/
+static int	parse_flags(int argc, char **argv, t_state *state)
+{
+	int	i;
+
+	i = 1;
+	state->strategy_flag = 0;
+	state->bench_mode = 0;
+	while (i < argc)
+	{
+		if (ft_strncmp(argv[i], "--", 2) == 0)
+			set_strategy(argv[i], state);
+		else
+			break ;
+		i++;
+	}
+	return (i);
+}
+
+/*
+** Gerekli hazırlıkları yapıp seçilen sıralama algoritmasını başlatır.
+*/
+static void	execute_sort(t_state *state)
+{
+	assign_ranks(state->a);
+	state->disorder = compute_disorder(state->a);
+	if (state->strategy_flag == 1)
+		sort_simple(state);
+	else if (state->strategy_flag == 2)
+		sort_medium(state);
+	else if (state->strategy_flag == 3)
+		sort_complex(state);
+	else
+		sort_adaptive(state);
+}
+
+/*
+** Programın ana giriş noktası (Orkestra Şefi)
+*/
 int	main(int argc, char **argv)
 {
-	static t_state	*state;
+	t_state	state;
+	int		arg_start;
 
-	(void)argc;
-	(void)argv;
-
-	state = new_state();
-	// stack_push_bottom(state->a, 20, 3);
-	// stack_push_bottom(state->a, 10, 1);
-	// stack_push_bottom(state->a, 21, 4);
-	// stack_push_bottom(state->a, 3,  0);
-	// stack_push_bottom(state->a, 14, 2);
-	stack_push_bottom(state->a, 5, 4);
-	stack_push_bottom(state->a, 4, 3);
-	stack_push_bottom(state->a, 3, 2);
-	stack_push_bottom(state->a, 2,  1);
-	stack_push_bottom(state->a, 1, 0);
-
-	sort_medium(state);
-
-
-	// t_node *node = state->a->top;
-	// while (1)
-	// {
-	// 	ft_printf("%d ", node->value);
-	// 	if (!node->next)
-	// 	{
-	// 		ft_printf("\n");
-	// 		break ;
-	// 	}
-	// 	node = node->next;
-	// }
-
-	sort_simple(state);
-
+	if (argc < 2)
+		return (0);
+	init_state(&state);
+	arg_start = parse_flags(argc, argv, &state);
+	if (!parse_numbers(&state, argc, argv, arg_start))
+	{
+		write(2, "Error\n", 6);
+		free_state(&state);
+		return (1);
+	}
+	if (state.a->size > 1 && !is_sorted(state.a))
+		execute_sort(&state);
+	if (state.bench_mode)
+		print_benchmark(&state);
+	free_state(&state);
 	return (0);
 }
