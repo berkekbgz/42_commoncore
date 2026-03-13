0ac5132e217b9a12c88829e4b03dd27d26618d23
Berke Emin Kabagoz <bkabagoz@k1m26s10.42istanbul.com.tr>
2026-03-13 21:11:44 +0300

    push_swap: compute disorder

--- 1_push_swap/compute_disorder.c
+++ 1_push_swap/compute_disorder.c
@@ -1,18 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort_complex.c                                     :+:      :+:    :+:   */
+/*   disorder.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bkabagoz <bkabagoz@student.42istanbul.com.tr>   +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/13 20:56:56 by bkabagoz          #+#    #+#             */
-/*   Updated: 2026/03/13 20:58:00 by bkabagoz         ###   ########.fr       */
+/*   Created: 2026/03/13 20:58:34 by bkabagoz          #+#    #+#             */
+/*   Updated: 2026/03/13 21:08:11 by bkabagoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sort_complex(t_state *state)
+double	compute_disorder(t_stack *stack)
 {
-	(void)state;
+	double	mistakes;
+	int		total_pairs;
+	t_node	*node_i;
+	t_node	*node_j;
+
+
+	node_i = stack->top;
+	total_pairs = 0;
+	mistakes = 0;
+	while (node_i)
+	{
+		node_j = node_i->next;
+		while (node_j)
+		{
+			total_pairs++;
+			if (node_i->rank > node_j->rank)
+				mistakes++;
+			node_j = node_j->next;
+		}
+		node_i = node_i->next;
+	}
+	if (total_pairs <= 0)
+		return (0);
+	return (mistakes / total_pairs);
 }
