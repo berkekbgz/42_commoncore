db9c25a0bbfb46263e7b6e3eb96f95fccc1aa725
Berke Emin Kabagoz <bkabagoz@k1m26s10.42istanbul.com.tr>
2026-03-13 20:56:14 +0300

    push_swap: add adaptive sort

--- 1_push_swap/sort_adaptive.c
+++ 1_push_swap/sort_adaptive.c
@@ -6,6 +6,18 @@
 /*   By: bkabagoz <bkabagoz@student.42istanbul.com.tr>   +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 12:33:36 by bkabagoz          #+#    #+#             */
-/*   Updated: 2026/03/08 13:11:27 by bkabagoz         ###   ########.fr       */
+/*   Updated: 2026/03/13 20:55:44 by bkabagoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sort_adaptive(t_state *state)
+{
+	if (state->disorder < 0.2)
+		return (sort_simple(state));
+	else if (state->disorder < 0.5)
+		return (sort_medium(state));
+	else
+		return (sort_complex(state));
+}
