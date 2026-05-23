2e048ca1314b3c4b2e88ec5cc9542844b30322ce
Berke Emin Kabagoz <bkabagoz@k1m06s07.42istanbul.com.tr>
2026-05-23 22:10:55 +0300

    fix(get_next_line): Mouilinette...

--- 1_get_next_line/get_next_line.c
+++ 1_get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bkabagoz <bkabagoz@student.42istanbul.com.tr>   +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 22:01:26 by bkabagoz          #+#    #+#             */
-/*   Updated: 2026/05/23 18:33:09 by bkabagoz         ###   ########.fr       */
+/*   Updated: 2026/05/23 22:10:07 by bkabagoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,8 +88,6 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (read(fd, 0, 0) < 0)
-		return (free_ptr(&remainder));
 	buf = malloc(sizeof(char) * ((size_t)BUFFER_SIZE + 1));
 	if (!buf)
 		return (NULL);
